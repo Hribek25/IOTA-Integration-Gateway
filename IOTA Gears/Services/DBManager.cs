7763c2349c344d0925fee016f8691ff60f0f314f
Hribek25 <petr@zizkovi.name>
2018-08-09 10:39:10 +0200

    Reporting lifted to CacheFilter

--- IOTA Gears/Services/DBManager.cs
+++ IOTA Gears/Services/DBManager.cs
@@ -29,25 +29,15 @@ namespace IOTA_Gears.Services
 
         public async Task<JsonResult> GetCacheEntryAsync(string request, string contentType, int LifeSpan)
         {
-            var cmd = "SELECT * FROM [cache] WHERE (cast(strftime('%s','now') as bigint)-cast([timestamp] as bigint))<=@span and [query]=@query and [response_type] like @contenttype";
-            var c = DBConnection.CreateCommand();
-            c.CommandText = cmd;
-            c.Parameters.AddRange(
-                new List<SqliteParameter>()
-                {
-                    new SqliteParameter("@span",LifeSpan),
-                    new SqliteParameter("@contenttype",contentType + "%"),
-                    new SqliteParameter("@query",request)
-                }
-            );
-            await DBConnection.OpenAsync();
+            SqliteCommand c = _GetCacheEntrySQL(request, contentType, LifeSpan);
+            DBConnection.Open();
 
             JsonResult cacheEntry = null; // default return value
             using (var reader = await c.ExecuteReaderAsync())
             {
                 if (reader.HasRows)
                 {
-                    await reader.ReadAsync(); // take the first one whatever it is
+                    reader.Read(); // take the first one whatever it is
                     var jsdata = (string)reader["response"];
                     cacheEntry = new JsonResult(
                         Newtonsoft.Json.JsonConvert.DeserializeObject(
@@ -58,33 +48,14 @@ namespace IOTA_Gears.Services
                 }
             }
             DBConnection.Close();
-            if (cacheEntry != null)
-            {
-                Logger.LogInformation("Cache entry was loaded from cache for Request: {request}, Lifespan = {LifeSpan}", request, LifeSpan);
-            }
-            else
-            {
-                Logger.LogInformation("No cache entry found in cache for Request: {request}, Lifespan = {LifeSpan}", request, LifeSpan);
-            }
-
             return cacheEntry;
         }
 
         //public JsonResult GetCacheEntry(string request, string contentType, int LifeSpan)
         //{
-        //    var cmd = "SELECT * FROM [cache] WHERE (cast(strftime('%s','now') as bigint)-cast([timestamp] as bigint))<=@span and [query]=@query and [response_type] like @contenttype";
-        //    var c = DBConnection.CreateCommand();
-        //    c.CommandText = cmd;
-        //    c.Parameters.AddRange(
-        //        new List<SqliteParameter>()
-        //        {
-        //            new SqliteParameter("@span",LifeSpan),
-        //            new SqliteParameter("@contenttype",contentType + "%"),
-        //            new SqliteParameter("@query",request)
-        //        }
-        //    );            
+        //    SqliteCommand c = _GetCacheEntrySQL(request, contentType, LifeSpan);
         //    DBConnection.Open();
-            
+
         //    JsonResult cacheEntry = null; // default return value
         //    using (var reader = c.ExecuteReader())
         //    {
@@ -101,52 +72,31 @@ namespace IOTA_Gears.Services
         //        }
         //    }
         //    DBConnection.Close();
-        //    if (cacheEntry != null)
-        //    {
-        //        Logger.LogInformation("Cache entry was loaded from cache for Request: {request}, Lifespan = {LifeSpan}", request, LifeSpan);
-        //    }
-        //    else
-        //    {
-        //        Logger.LogInformation("No cache entry found in cache for Request: {request}, Lifespan = {LifeSpan}", request, LifeSpan);
-        //    }
             
         //    return cacheEntry;
         //}
+              
+        public async Task AddCacheEntryAsync(string request, JsonResult result, string contentType)
+        {
+            SqliteCommand c = _AddCacheEntrySQL(request, result, contentType);
+
+            DBConnection.Open();
+            await c.ExecuteNonQueryAsync();
+            DBConnection.Close();
+            
+        }
 
         //public void AddCacheEntry(string request, JsonResult result, string contentType)
         //{
-        //    var cmd = "DELETE FROM [cache] WHERE [query]=@query and [response_type] like @response_type;" + Environment.NewLine; // delete old entries from cache for the given query type/content type
-        //    cmd += "INSERT INTO [cache] ([timestamp],[query], [response_type], [response]) VALUES (strftime('%s','now'),@query, @response_type, @response)";
-
-        //    var c = DBConnection.CreateCommand();
-        //    c.CommandText = cmd;
-
-        //    var json = Newtonsoft.Json.JsonConvert.SerializeObject(
-        //        result.Value,
-        //        Newtonsoft.Json.Formatting.None,
-        //        new JsonSerializerSettings() { PreserveReferencesHandling = PreserveReferencesHandling.All, TypeNameHandling = TypeNameHandling.All }
-        //        );
-
-        //    c.Parameters.AddRange(
-        //        new List<SqliteParameter>()
-        //        {
-        //            new SqliteParameter("@query",request),
-        //            //new SqliteParameter("@span",LifeSpan),
-        //            new SqliteParameter("@response_type",contentType),
-        //            new SqliteParameter("@response", json)
-        //            //new SqliteParameter("@data", SqliteType.Blob,binObj.Length ) { Value=binObj}
-        //        }
-        //    );
+        //    SqliteCommand c = _AddCacheEntrySQL(request, result, contentType);
 
         //    DBConnection.Open();
         //    c.ExecuteNonQuery();
         //    DBConnection.Close();
-        //    Logger.LogInformation("New entry to CACHE table for Request: {request}", request);
-                        
-        //    return ;
         //}
 
-        public async Task AddCacheEntryAsync(string request, JsonResult result, string contentType)
+        #region SQLHelpers
+        private SqliteCommand _AddCacheEntrySQL(string request, JsonResult result, string contentType)
         {
             var cmd = "DELETE FROM [cache] WHERE [query]=@query and [response_type] like @response_type;" + Environment.NewLine; // delete old entries from cache for the given query type/content type
             cmd += "INSERT INTO [cache] ([timestamp],[query], [response_type], [response]) VALUES (strftime('%s','now'),@query, @response_type, @response)";
@@ -170,14 +120,25 @@ namespace IOTA_Gears.Services
                     //new SqliteParameter("@data", SqliteType.Blob,binObj.Length ) { Value=binObj}
                 }
             );
-
-            await DBConnection.OpenAsync();
-            await c.ExecuteNonQueryAsync();
-            DBConnection.Close();
-            Logger.LogInformation("New entry to CACHE table for Request: {request}", request);
-                        
+            return c;
         }
 
+        private SqliteCommand _GetCacheEntrySQL(string request, string contentType, int LifeSpan)
+        {
+            var cmd = "SELECT * FROM [cache] WHERE (cast(strftime('%s','now') as bigint)-cast([timestamp] as bigint))<=@span and [query]=@query and [response_type] like @contenttype";
+            var c = DBConnection.CreateCommand();
+            c.CommandText = cmd;
+            c.Parameters.AddRange(
+                new List<SqliteParameter>()
+                {
+                    new SqliteParameter("@span",LifeSpan),
+                    new SqliteParameter("@contenttype",contentType + "%"),
+                    new SqliteParameter("@query",request)
+                }
+            );
+            return c;
+        }
+        #endregion
 
     }
 }
