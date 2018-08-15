f7e6005b17bdfd28071f6c8883c374d98ddd7e74
Hribek25 <petr@zizkovi.name>
2018-08-15 18:35:41 +0200

    Additional custom JSON Converters

--- IOTA Gears/DBSerializer.cs
+++ IOTA Gears/DBSerializer.cs
@@ -8,7 +8,8 @@ using Tangle.Net.Entity;
 
 namespace IOTA_Gears
 {
-    #region JsonConverters
+    #region CustomizedJsonConverters
+    // based on https://stackoverflow.com/a/23017892
 
     class TangleAddressConverter : JsonConverter
     {
@@ -25,7 +26,7 @@ namespace IOTA_Gears
             // Read the properties which will be used as constructor parameters
             string value = (string)jo["Value"];
             string checksum = "";
-            if (jo["Checksum"]["Value"].Type == JTokenType.String)
+            if (jo["Checksum"].Contains("Value") && jo["Checksum"]["Value"].Type == JTokenType.String)
             {
                 checksum = (string)jo["Checksum"]["Value"];
             }
@@ -121,23 +122,141 @@ namespace IOTA_Gears
             throw new NotImplementedException();
         }
     }
+    class TangleFragmentConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(Tangle.Net.Entity.Fragment));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            // Load the JSON for the Result into a JObject
+            JObject jo = JObject.Load(reader);
+
+            // Read the properties which will be used as constructor parameters
+            string value = (string)jo["Value"];
+
+            // Construct the Result object using the non-default constructor
+            Fragment result = new Fragment(value);
+
+            // (If anything else needs to be populated on the result object, do that here)
+
+            // Return the result
+            return result;
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class TangleTagConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(Tangle.Net.Entity.Tag));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            // Load the JSON for the Result into a JObject
+            JObject jo = JObject.Load(reader);
+
+            // Read the properties which will be used as constructor parameters
+            string value = (string)jo["Value"];
+
+            // Construct the Result object using the non-default constructor
+            Tag result = new Tag(value);
+
+            // (If anything else needs to be populated on the result object, do that here)
+
+            // Return the result
+            return result;
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    //class TangleTransactionConverter : JsonConverter
+    //{
+    //    public override bool CanConvert(Type objectType)
+    //    {
+    //        return (objectType == typeof(Tangle.Net.Entity.Transaction));
+    //    }
+
+    //    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    //    {
+    //        // Load the JSON for the Result into a JObject
+
+    //        if (reader.TokenType == JsonToken.String)
+    //        {
+    //            // Read the properties which will be used as constructor parameters
+    //            var value = (string)reader.Value;
+
+    //            // Construct the Result object using the non-default constructor
+    //            var result = Transaction.FromTrytes(new TransactionTrytes(value));
+
+    //            // (If anything else needs to be populated on the result object, do that here)
+    //            // Return the result
+    //            return result;
+    //        }
+
+    //        return null;            
+    //    }
+
+    //    public override bool CanWrite
+    //    {
+    //        get { return true; }
+    //    }
+
+    //    public override bool CanRead
+    //    {
+    //        get { return true; }
+    //    }
+
+    //    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    //    {
+    //        var o = (Transaction)value;
+
+    //        // Transactions are serialized as Trytes
+    //        var jt = new JValue(o.ToTrytes().Value);
+    //        jt.WriteTo(writer);
+    //    }
+    //}
 
     #endregion
-
 
     public class DBSerializer
     {
         public static string SerializeToJson(object input)
         {
+            var settings = new JsonSerializerSettings()
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.All,
+                TypeNameHandling = TypeNameHandling.All
+            };
+                       
+
             string res;
             res = JsonConvert.SerializeObject(
                 input,
                 Formatting.None,
-                new JsonSerializerSettings() {
-                    PreserveReferencesHandling = PreserveReferencesHandling.All,
-                    TypeNameHandling = TypeNameHandling.All,
-                    ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor
-                }
+                settings
             );
 
             return res;
@@ -154,6 +273,8 @@ namespace IOTA_Gears
             settings.Converters.Add(new TangleAddressConverter());
             settings.Converters.Add(new TangleTryteStringConverter());
             settings.Converters.Add(new TangleHashConverter());
+            settings.Converters.Add(new TangleFragmentConverter());
+            settings.Converters.Add(new TangleTagConverter());
             
             var res =  JsonConvert.DeserializeObject( input, settings );
 
