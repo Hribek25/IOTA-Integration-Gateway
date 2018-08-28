9d03e6177f0a6d44fb68f46a2148fc27735c6864
Hribek25 <petr@zizkovi.name>
2018-08-28 15:26:18 +0200

    IsValidAddress function moved to separate class

---
+++ IOTA Gears/CommonHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace IOTA_Gears
+{
+    public class CommonHelpers
+    {
+        public static bool IsValidAddress(string address) =>
+            Regex.IsMatch(address, @"^(([A-Z9]{90})|([A-Z9]{81}))$");
+    }
+}
