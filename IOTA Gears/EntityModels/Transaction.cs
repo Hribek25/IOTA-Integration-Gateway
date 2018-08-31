1e294c6978e20b27737efc3f164bcad9c775b255
Hribek25 <petr@zizkovi.name>
2018-08-31 17:08:45 +0200

    Added Transaction container

---
+++ IOTA Gears/EntityModels/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOTA_Gears.EntityModels
+{
+    public class TransactionContainer
+    {
+        public Tangle.Net.Entity.Transaction Transaction { get; set; }
+        public bool? IsConfirmed { get; set; } = null;
+
+        public TransactionContainer()
+        {
+            // default ctor because of deserialization
+        }
+
+        public TransactionContainer(Tangle.Net.Entity.Transaction transaction)
+        {
+            Transaction = transaction;
+        }
+
+        public TransactionContainer(Tangle.Net.Entity.TransactionTrytes trytes)
+        {
+            Transaction = Tangle.Net.Entity.Transaction.FromTrytes(trytes);
+        }
+    }
+}
