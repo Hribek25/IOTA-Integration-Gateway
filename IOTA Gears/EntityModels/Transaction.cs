b3dc1d8b11d7306c52a4a0f06f19349dfa800a3b
Hribek25 <petr@zizkovi.name>
2018-09-01 01:33:37 +0200

    New transaction enums

--- IOTA Gears/EntityModels/Transaction.cs
+++ IOTA Gears/EntityModels/Transaction.cs
@@ -1,14 +1,32 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace IOTA_Gears.EntityModels
 {
+    public enum TransactionFilter
+    {
+        ConfirmedOnly,
+        All        
+    }
+
+    public enum TransactionType
+    {
+        ValueTransaction,
+        NonValueTransaction
+    }
+
     public class TransactionContainer
     {
         public Tangle.Net.Entity.Transaction Transaction { get; set; }
         public bool? IsConfirmed { get; set; } = null;
+        public string DecodedMessage { get; set; } = null;
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TransactionType TransactionType { get; set; }
 
         public TransactionContainer()
         {
@@ -18,11 +36,13 @@ namespace IOTA_Gears.EntityModels
         public TransactionContainer(Tangle.Net.Entity.Transaction transaction)
         {
             Transaction = transaction;
+            this.DecodedMessage = !Transaction.Fragment.IsEmpty ? Transaction.Fragment.ToUtf8String() : null;
+            this.TransactionType = Transaction.Value != 0 ? TransactionType.ValueTransaction : TransactionType.NonValueTransaction;
         }
 
-        public TransactionContainer(Tangle.Net.Entity.TransactionTrytes trytes)
+        public TransactionContainer(Tangle.Net.Entity.TransactionTrytes trytes) : this(Tangle.Net.Entity.Transaction.FromTrytes(trytes))
         {
-            Transaction = Tangle.Net.Entity.Transaction.FromTrytes(trytes);
-        }
+
+        }        
     }
 }
