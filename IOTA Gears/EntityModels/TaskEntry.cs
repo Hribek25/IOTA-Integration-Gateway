c3bc609f452bb4ff9f2201d0336a2d6f5f544b79
Hribek25 <petr@zizkovi.name>
2018-09-06 22:57:47 +0200

    Task pipe line entity

---
+++ IOTA Gears/EntityModels/TaskEntry.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOTA_Gears.EntityModels
+{
+    public class TaskEntry
+    {
+        public long Rowid { get; set; }
+        public TaskEntryInput Input { get; set; }
+        public string Task { get; set; }
+        public long Timestamp { get; set; }
+        public string GuId { get; set; }
+    }
+
+    public class PipelineStatus
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public StatusDetail Status { get; set; }
+
+        public string GuId { get; set; }
+    }
+
+    public enum StatusDetail
+    {
+        TaskWasAddedToPipeline,
+        TooManyRequests
+    }
+
+    public class TaskEntryInput
+    {
+        public string Address { get; set; }
+        public string Message { get; set; }
+        public string Tag { get; set; }
+    }
+
+}
