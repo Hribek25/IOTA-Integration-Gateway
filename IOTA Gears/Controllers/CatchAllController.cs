f57bcb9945e61d2d08b39fdbbd447b5e2893ee38
Hribek25 <petr@zizkovi.name>
2018-09-18 19:25:58 +0200

    Added CatchAll wireframe

---
+++ IOTA Gears/Controllers/CatchAllController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IOTAGears.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class CatchAllController : Controller
+    {   
+        [Route("{*url}",Order = 999)]        
+        public IActionResult CatchAll()
+        {
+            return View();
+        }
+    }
+}
